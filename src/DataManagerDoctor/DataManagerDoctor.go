521523734e5bed548574bf76607b0092b2712da9
mertovich <88425310+mertovich@users.noreply.github.com>
2022-04-06 17:14:38 +0300

    add(DataManagerDoctor): Doctor update gender

--- src/DataManagerDoctor/DataManagerDoctor.go
+++ src/DataManagerDoctor/DataManagerDoctor.go
@@ -158,3 +158,20 @@ func UpdateAge(doctorAge int, doctorTcNo int) {
 	smtm.Exec(doctorAge, doctorTcNo)
 	tx.Commit()
 }
+
+func UpdateGender(doctorGender string, doctorTcNo int) {
+	db, err := sql.Open("sqlite3", "./database.db")
+	if err != nil {
+		defer db.Close()
+	}
+
+	sqlStmt := `
+	CREATE TABLE IF NOT EXISTS  doctors(Doctor_TC_NO integer not null primary key, Doctor_Password text, Doctor_Name text, Doctor_Last_Name text, Doctor_Age integer, Doctor_Gender text, Doctor_Hes_Code text, Doctor_Phone_Number integer,Doctor_Mail text, Doctor_Expertise text);
+	`
+	db.Exec(sqlStmt)
+
+	tx, _ := db.Begin()
+	smtm, _ := tx.Prepare("UPDATE doctors SET Doctor_Gender = ? WHERE Doctor_TC_NO = ?")
+	smtm.Exec(doctorGender, doctorTcNo)
+	tx.Commit()
+}
